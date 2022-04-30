d16ef7b684b9ed98157e517aae1d328748fe952d
yunine16 <yuchin616@icloud.com>
2022-05-01 04:14:17 +0900

    port番号修正

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const http = require('http');
-const fs = require('fs');
-const { url } = require('inspector');
+const http = require("http");
+const fs = require("fs");
+const { url } = require("inspector");
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = "127.0.0.1";
+const port = process.env.PORT || 3000;
 
 const server = http.createServer(RouteSetting);
 
@@ -13,50 +13,50 @@ server.listen(port, hostname, () => {
 
 function RouteSetting(req, res) {
   switch (true) {
-    case '/' === req.url:
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      const indexPage  = fs.readFileSync('./index.html', 'UTF-8');
+    case "/" === req.url:
+      res.writeHead(200, { "Content-Type": "text/html" });
+      const indexPage = fs.readFileSync("./index.html", "UTF-8");
       res.write(indexPage);
       res.end();
       break;
 
     case /\.html$/.test(req.url):
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      const htmlFile  = fs.readFileSync('.'+req.url, 'UTF-8');
+      res.writeHead(200, { "Content-Type": "text/html" });
+      const htmlFile = fs.readFileSync("." + req.url, "UTF-8");
       res.write(htmlFile);
       res.end();
       break;
-    
+
     case /\.css$/.test(req.url):
-      res.writeHead(200, {'Content-Type': 'text/css'});
-      const cssFile  = fs.readFileSync('.'+req.url, 'UTF-8');
+      res.writeHead(200, { "Content-Type": "text/css" });
+      const cssFile = fs.readFileSync("." + req.url, "UTF-8");
       res.write(cssFile);
       res.end();
       break;
 
     case /\.js$/.test(req.url):
-      res.writeHead(200, {'Content-Type': 'text/plain'});
-      const jsFile = fs.readFileSync('.'+req.url, 'UTF-8');
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      const jsFile = fs.readFileSync("." + req.url, "UTF-8");
       res.write(jsFile);
       res.end();
       break;
-    
+
     case /\.png$/.test(req.url):
-      res.writeHead(200, {'Content-Type': "image/png"});
-      const pngFile = fs.readFileSync('.'+req.url);
+      res.writeHead(200, { "Content-Type": "image/png" });
+      const pngFile = fs.readFileSync("." + req.url);
       res.end(pngFile);
       break;
 
     case /\.ico$/.test(req.url):
-      res.writeHead(200, {'Content-Type': "image/x-icon"});
-      const icoFile = fs.readFileSync('.'+req.url);
+      res.writeHead(200, { "Content-Type": "image/x-icon" });
+      const icoFile = fs.readFileSync("." + req.url);
       res.end(icoFile);
       break;
 
     default:
-      res.setHeader('Content-Type', 'text/plain; charset=utf-8');
+      res.setHeader("Content-Type", "text/plain; charset=utf-8");
       console.log(req.url);
-      res.write('お探しのページは見つかりません。');
+      res.write("お探しのページは見つかりません。");
       res.end();
       break;
   }
